1ae60ac590f04a6db9b6ab9de223e5ca2eb9fbde
BJoarder <bjoarder87@gmail.com>
2023-07-23 00:57:38 +0600

    json data added to mongodb & get data from mongodb

--- index.js
+++ index.js
@@ -27,6 +27,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const collegesCollection = client.db('collegeBookr').collection('collegesData');
+
+    app.get('/colleges', async(req, res) => {
+      const cursor = collegesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
